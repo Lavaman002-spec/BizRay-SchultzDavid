a939617ffb4242f03a29076489e032f39014fd58
Cozmin <Cozmin.r.cr@gmail.com>
2025-10-16 12:58:45 +0200

    added hopemage

---
+++ src/components/layout/AppNav.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import * as React from 'react';
+import Link from 'next/link';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from '@/components/ui/navigation-menu';
+import Image from 'next/image';
+import { Button } from '../ui/button';
+
+export function AppNav() {
+  return (
+    <header className="border-b fixed w-dvw">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <Link href="/" className="flex items-center gap-2 text-xl font-bold">
+          <Image
+            src="/company-logo.svg"
+            width={100}
+            height={48}
+            alt="Company Logo"
+          />
+        </Link>
+        {/* Nav Menu */}
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <NavigationMenu>
+            <NavigationMenuLink>
+              <Link href="/dashboard">Dashbaord</Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink>
+              <Link href="/explore">Explore</Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink>
+              <Link href="/network">Network</Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink>
+              <Link href="/settings">Settings</Link>
+            </NavigationMenuLink>
+          </NavigationMenu>
+        </div>
+
+        {/* CTAs */}
+        <div className="flex gap-2">
+          <Button variant={'secondary'}>Log In</Button>
+          <Button>Create Account</Button>
+        </div>
+      </div>
+    </header>
+  );
+}
